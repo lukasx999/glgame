bbc322d3c8cc84b844e49d161396bdfa915ebfda
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-10-12 00:43:12 +0200

    python script for embedding shaders

---
+++ libgfx/shaders/shaders.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace shaders {
+
+namespace vertex {
+
+inline constexpr char default_[] = {
+#embed "../shaders/default.vert" suffix(,)
+'\0'
+};
+
+inline constexpr char rectangle[] = {
+#embed "../shaders/rectangle.vert" suffix(,)
+'\0'
+};
+
+inline constexpr char texture[] = {
+#embed "../shaders/texture.vert" suffix(,)
+'\0'
+};
+
+} // namespace vertex
+
+namespace fragment {
+
+inline constexpr char circle[] = {
+#embed "../shaders/circle.frag" suffix(,)
+'\0'
+};
+
+inline constexpr char default_[] = {
+#embed "../shaders/default.frag" suffix(,)
+'\0'
+};
+
+inline constexpr char texture[] = {
+#embed "../shaders/texture.frag" suffix(,)
+'\0'
+};
+
+inline constexpr char rectangle[] = {
+#embed "../shaders/rectangle.frag" suffix(,)
+'\0'
+};
+
+} // namespace fragment
+
+} // namespace shaders
