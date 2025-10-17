7e580969ae5d4135a35ad44c74c454641833996b
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-10-18 00:17:29 +0200

    extracted libgfx into its own repo

--- libgfx/include/gfx.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <renderer.hh>
-#include <window.hh>
-#include <types.hh>
-#include <vec.hh>
--- libgfx/src/gfx.cc
+++
@@ -1,9 +0,0 @@
-// has to be included before defining GLAD_GL_IMPLEMENTATION, as we'd otherwise
-// get 2 implementations
-#include <gfx.hh>
-
-#define GLAD_GL_IMPLEMENTATION
-#include <glad/gl.h>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
